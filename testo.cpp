57a4ad7ea938f1fdaa2851b85682604ad4a686da
davidberard98 <david@kooia.info>
2013-07-25 11:31:23 -0700

    going to try branches again

---
+++ testo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+<<<<<<< HEAD
+using namespace std;
+
+int main()
+{
+cout << "This is a branch of the main github test repository" << endl;
+
+=======
+int main()
+{
+std::cout << "testing github" << std::endl;
+>>>>>>> BranchTest
+return 0;
+}
